653ee44e7ef912522c81ae238fae3a83b3f01fe5
phrazzld <phrazzld@pm.me>
2025-05-16 10:01:26 -0500

    cleanup: remove test file used for pre-commit hook verification

--- test-eslint-violation.ts
+++
@@ -1,4 +0,0 @@
-// This file contains a fixed ESLint violation to verify pre-commit hook allows clean commits
-export function testFunction(param: string): string {
-  return param.toUpperCase();
-}
